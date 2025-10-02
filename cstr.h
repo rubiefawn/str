609329940dab8ec40f799b3b77f9709e96371680
Fawn <rubiefawn@gmail.com>
2025-10-02 00:33:05 -0600

    Add some comments to headers

--- cstr.h
+++ cstr.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <stddef.h>
 
-// strncpy, except result is always guaranteed to be null-terminated
-// returns remaining free space, which will be negative if the string was truncated
+// Similar to `strncpy()`, except result is always null-terminated
+// Returns remaining free space. If the string was truncated, this
+// amount will be negative, whose absolute value is equal to the number
+// of characters that did not fit.
 ptrdiff_t strzcpy(char *restrict dest, const char *restrict src, size_t size);
 
-// strncat, except result is always guaranteed to be null-terminated
-// returns remaining free space, which will be negative if the string was truncated
+// Similar to `strncat()`, except result is always null-terminated
+// Returns remaining free space. If the string was truncated, this
+// amount will be negative, whose absolute value is equal to the number
+// of characters that did not fit.
 ptrdiff_t strzcat(char *restrict dest, const char *restrict src, size_t size);
 
 #if !defined(__unix__) && !defined(__STDC_ALLOC_LIB__) && __STDC_WANT_LIB_EXT2__ != 1
--- str.h
+++ str.h
@@ -2,32 +2,63 @@
 
 typedef struct str { char *data; int len; } str;
 
+// Constructs a non-owning `str` from a `char*`.
 str str_from_cstr(char *sz);
 
+// Returns a non-owning copy of a `str` with all leading and trailing whitespace removed.
 str str_strip_whitespace(str it);
+
+// Returns a non-owning copy of a `str` with all leading whitespace removed.
 str str_strip_whitespace_pre(str it);
+
+// Returns a non-owning copy of a `str` with all trailing whitespace removed.
 str str_strip_whitespace_post(str it);
 
+// Returns a non-owning copy of a `str` with a given prefix removed, if it is present.
 str str_strip_prefix_str(str it, str prefix);
+
+// Returns a non-owning copy of a `str` with a given postfix removed, if it is present.
 str str_strip_postfix_str(str it, str postfix);
+
+// Returns a non-owning copy of a `str` with a given prefix removed, if it is present.
 str str_strip_prefix_cstr(str it, const char *prefix);
+
+// Returns a non-owning copy of a `str` with a given postfix removed, if it is present.
 str str_strip_postfix_cstr(str it, const char *postfix);
 
 #define str_strip_prefix(IT, PREFIX) _Generic((PREFIX), str: str_strip_prefix_str, char *: str_strip_prefix_cstr)((IT), (PREFIX))
 
 #define str_strip_postfix(IT, POSTFIX) _Generic((POSTFIX), str: str_strip_postfix_str, char *: str_strip_postfix_cstr)((IT), (POSTFIX))
 
+// TODO: Make strcmp equivalents that measure difference instead of just returning a bool, since sometimes that is useful
+
+// Checks if a `str` is null-terminated.
 bool str_is_sz(str it);
+
+// Compares two `str` to see if their contents are equal.
 bool str_eq_str(str a, str b);
+
+// Compares a `str` and a `char*` to see if their contents are equal.
 bool str_eq_cstr(str a, const char *b);
+
+// Compares a `str` and a `char*` to see if their contents are equal.
 bool cstr_eq_str(const char *a, str b);
+
+// Compares two `char*` to see if their contents are equal.
 bool cstr_eq_cstr(const char *a, const char *b);
 
 #define str_eq(LHS, RHS) _Generic((LHS), str: _Generic((RHS), str: str_eq_str, char *: str_eq_cstr), char *: _Generic((RHS), str: cstr_eq_str, char *: cstr_eq_cstr))((LHS), (RHS))
 
+// Compares a `str` to several other `str`s to see if its contents are equal to any of those options.
 bool str_eq_str_any(str it, int count, ...);
+
+// Compares a `str` to several `char*`s to see if its contents are equal to any of those options.
 bool str_eq_cstr_any(str it, int count, ...);
+
+// Compares a `char*` to several `str`s to see if its contents are equal to any of those options.
 bool cstr_eq_str_any(const char *it, int count, ...);
+
+// Compares a `char*` to several other `char*`s to see if its contents are equal to any of those options.
 bool cstr_eq_cstr_any(const char *it, int count, ...);
 
 #define str_eq_any(LHS, COUNT, RHS, ...) _Generic((LHS), str: _Generic((RHS), str: str_eq_str_any, char *: str_eq_cstr_any), char *: _Generic((RHS), str: cstr_eq_str_any, char *: cstr_eq_cstr_any))((LHS), (COUNT), (RHS) __VA_OPT(, __VA_ARGS__))
