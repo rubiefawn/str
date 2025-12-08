8aac5f7286e8775ea7f76ae07fa36a68fe5ba8d8
Fawn <rubiefawn@gmail.com>
2025-12-07 21:43:03 -0700

    Fix strndup(3) polyfill
    The polyfill previously allocated space equal to the max length
    parameter for some reason, which was incredibly wasteful. This is no
    longer the case. Now the allocation size is properly determined *before*
    the allocation is made.

--- cstr.c
+++ cstr.c
@@ -14,23 +14,24 @@ ptrdiff_t strzcat(char *restrict dest, const char *restrict src, size_t size) {
 	return strzcpy(&dest[dest_len], src, size - dest_len);
 }
 
-#if !defined(__unix__) && !defined(__STDC_ALLOC_LIB__) && __STDC_WANT_LIB_EXT2__ != 1
+#if !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L
 #include <stdlib.h>
 
 char *strdup(const char *src) {
 	const size_t len = strlen(src) + 1;
-	char *dest = calloc(1, len);
-	if (!dest) return nullptr;
+	char *dest = calloc(sizeof(char), len);
+	if (!dest) { return nullptr; }
 	memcpy(dest, src, len);
 	return dest;
 }
 
 char *strndup(const char *src, size_t size) {
-	char *dest = calloc(1, size + 1);
-	if (!dest) return nullptr;
-	const size_t src_len = strlen(src);
-	const size_t cpy_len = src_len < size ? src_len : size;
-	memcpy(dest, src, cpy_len);
+	const char *end = memchr(src, '\0', size);
+	const size_t len = end ? end - src : size;
+	char *dest = calloc(sizeof(char), len + 1);
+	if (!dest) { return nullptr; }
+	memcpy(dest, src, len);
+	dest[len] = '\0';
 	return dest;
 }
 #endif
--- cstr.h
+++ cstr.h
@@ -13,7 +13,7 @@ ptrdiff_t strzcpy(char *restrict dest, const char *restrict src, size_t size);
 // of characters that did not fit.
 ptrdiff_t strzcat(char *restrict dest, const char *restrict src, size_t size);
 
-#if !defined(__unix__) && !defined(__STDC_ALLOC_LIB__) && __STDC_WANT_LIB_EXT2__ != 1
+#if !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L
 char *strdup(const char *src);
 char *strndup(const char *src, size_t size);
 #endif
